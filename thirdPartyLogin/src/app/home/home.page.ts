f4b073ad9e24e29e6eea47b5a144be11f37b656f
Hongyi Syue <homeyxue@outlook.com>
2020-07-14 16:10:26 -0600

    microsoft auth key working

--- thirdPartyLogin/src/app/home/home.page.ts
+++ thirdPartyLogin/src/app/home/home.page.ts
@@ -10,58 +10,59 @@ export class HomePage {
 
   @ViewChild('pwaAuthElement') pwaAuthElement: ElementRef;
 
-  ionViewDidEnter(){
+  ionViewDidEnter() {
     this.pwaAuthElement.nativeElement.addEventListener("signin-completed", (e: CustomEvent) => {
-    if (!e.detail.error) {
-      const signIn = e.detail;
-      
-      // console.log("Email: ", signIn.email);
-      console.log("Name: ", signIn.name);
-      console.log("Picture: ", signIn.imageUrl);
-      console.log("Access token", signIn.accessToken);
-      console.log("Access token expiration date", signIn.accessTokenExpiration);
-      console.log("Provider (MS, Google, FB): ", signIn.provider);
-      console.log("Raw data from provider: ", signIn.providerData);
-      // Sign-in successful! 
-      const email = e.detail.email;
-      const name = e.detail.name;
-      const imageUrl = e.detail.imageUrl;    
-      
-      // Hide the sign-in button
-      this.pwaAuthElement.nativeElement.style.display = "none"; 
-      
-      // Tell the user they're signed in
-      const messageElement = document.createElement("h1");
-      messageElement.textContent = "You're signed-in 😎";
-      document.body.appendChild(messageElement);
-      
-      // Show their profile picture
-      if (imageUrl) {
-        const imageElement = document.createElement("img");
-        imageElement.src = imageUrl;
-        document.body.append(imageElement);
+      // if(!e.detail.error) { //this doesn't work for MicroSoft login
+      if (e.detail.providerData) {
+        const signIn = e.detail;
+
+        // console.log("Email: ", signIn.email);
+        console.log("Name: ", signIn.name);
+        console.log("Picture: ", signIn.imageUrl);
+        console.log("Access token", signIn.accessToken);
+        console.log("Access token expiration date", signIn.accessTokenExpiration);
+        console.log("Provider (MS, Google, FB): ", signIn.provider);
+        console.log("Raw data from provider: ", signIn.providerData);
+        // Sign-in successful! 
+        const email = e.detail.email;
+        const name = e.detail.name;
+        const imageUrl = e.detail.imageUrl;
+
+        // Hide the sign-in button
+        this.pwaAuthElement.nativeElement.style.display = "none";
+
+        // Tell the user they're signed in
+        const messageElement = document.createElement("h1");
+        messageElement.textContent = "You're signed-in 😎";
+        document.body.appendChild(messageElement);
+
+        // Show their profile picture
+        if (imageUrl) {
+          const imageElement = document.createElement("img");
+          imageElement.src = imageUrl;
+          document.body.append(imageElement);
+        }
+
+        // Show their name
+        const nameElement = document.createElement("h3");
+        nameElement.textContent = name;
+        document.body.append(nameElement);
+
+        // Show their email
+        const emailElement = document.createElement("h3");
+        emailElement.textContent = email;
+        document.body.append(emailElement);
+
+        // Show how they signed in
+        const providerElement = document.createElement("p");
+        providerElement.textContent = `Signed in through ${e.detail.provider}`;
+        document.body.append(providerElement);
+      } else {
+        // An error occurred
+        document.body.append("An error occurred: " + JSON.stringify(e.detail.error));
       }
-      
-      // Show their name
-      const nameElement = document.createElement("h3");
-      nameElement.textContent = name;
-      document.body.append(nameElement);
-      
-      // Show their email
-      const emailElement = document.createElement("h3");
-      emailElement.textContent = email;
-      document.body.append(emailElement);
-      
-      // Show how they signed in
-      const providerElement = document.createElement("p");
-      providerElement.textContent = `Signed in through ${e.detail.provider}`;
-      document.body.append(providerElement);
-    } else {
-      // An error occurred
-      document.body.append("An error occurred: " + JSON.stringify(e.detail.error));
-    }
-  });
+    });
   }
-  constructor() {}
+  constructor() { }
 
 }
